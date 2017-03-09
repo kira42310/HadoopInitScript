cd317e8cfdc77ad7e891c4e68af6fb4e6f491c3d
kira4231 <soratouch@hotmail.com>
2017-03-10 00:12:19 +0700

    Modify benchmark.py

--- benchmark.py
+++ benchmark.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 
-print ("test")
+import sys
+
+if len(sys.argv) <= 1:
+    print('No arguments supplied.')
+    exit()
+
+def main():
+    '''
+    Main function
+    '''
+    for turn in range(0, 5):
+        print('test', turn)
+
+
+
+if __name__ == '__main__':
+    main()
